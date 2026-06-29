28127dea4b2d8106b3ba117c9f73a7909abf6b10
isiakpona_church <isiakpona@churchofjesuschrist.org>
2026-06-29 15:29:52 +0100

    cleaning, debugging for deployment including social handle placeholders

--- js/main.js
+++ js/main.js
@@ -34,36 +34,3 @@ card.style.display='none';
 });
 });
 
-const lightbox=document.getElementById('lightbox');
-const lightboxImg=document.getElementById('lightbox-img');
-
-document.querySelectorAll('.card img').forEach(img=>{
-
-img.addEventListener('click',()=>{
-
-lightbox.style.display='flex';
-lightboxImg.src=img.src;
-
-});
-
-});
-
-lightbox.addEventListener('click',()=>{
-lightbox.style.display='none';
-});
-
-
-
-
-
-
-
-/* NAVIGATION */
-
-/* COUNTERS */
-
-/* SCROLL ANIMATIONS */
-
-/* GALLERY */
-
-/* CONTACT */
